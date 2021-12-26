9f22db1d4388ba00b2ef9178b6659e8bcd79c3ed
Harry <harrydstafford@gmail.com>
2021-12-26 03:22:50 +0200

    Fix apps starting at correct workspace
    Dwm's check for the starting app's title is case sensitive. Changing it
    to the title that's usually displayed on the status bar will usually be
    correct.

--- dwm/.local/src/dwm/config.h
+++ dwm/.local/src/dwm/config.h
@@ -28,9 +28,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "discord",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "qjackctl", NULL,       NULL,       1 << 7,       1,           -1 },
-	{ "spotify",  NULL,       NULL,       1 << 7,       0,           -1 },
+        { "discord",  NULL,       NULL,       1 << 8,       0,           -1 },
+        { "Steam",    NULL,       NULL,       1 << 6,       0,           -1 },
+        { "QjackCtl", NULL,       NULL,       1 << 7,       1,           -1 },
 };
 
 /* layout(s) */
