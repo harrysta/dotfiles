60bdd54b76f05a8322440b52700099563c2fa752
Harry Stafford <harrydstafford@gmail.com>
2022-01-23 22:19:20 +0200

    Replace terminal and vim colortheme with Tokyonight

--- nvim/.config/nvim/modules/plug_essential.vim
+++ nvim/.config/nvim/modules/plug_essential.vim
@@ -4,3 +4,4 @@ Plug 'qpkorr/vim-bufkill'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-telescope/telescope-rg.nvim'
+Plug 'folke/tokyonight.nvim', { 'branch': 'main' }
--- st/.local/src/st/config.h
+++ st/.local/src/st/config.h
@@ -93,41 +93,43 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+#include "tokyo_night.h"
+
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
+// static const char *colorname[] = {
 
-  /* 8 normal colors */
-  [0] = "#2b303b", /* black   */
-  [1] = "#bf616a", /* red     */
-  [2] = "#a3be8c", /* green   */
-  [3] = "#ebcb8b", /* yellow  */
-  [4] = "#8fa1b3", /* blue    */
-  [5] = "#b48ead", /* magenta */
-  [6] = "#96b5b4", /* cyan    */
-  [7] = "#c0c5ce", /* white   */
+  // [> 8 normal colors <]
+  // [0] = "#2b303b", [> black   <]
+  // [1] = "#bf616a", [> red     <]
+  // [2] = "#a3be8c", [> green   <]
+  // [3] = "#ebcb8b", [> yellow  <]
+  // [4] = "#8fa1b3", [> blue    <]
+  // [5] = "#b48ead", [> magenta <]
+  // [6] = "#96b5b4", [> cyan    <]
+  // [7] = "#c0c5ce", [> white   <]
 
-  /* 8 bright colors */
-  [8]  = "#65737e", /* black   */
-  [9]  = "#bf616a", /* red     */
-  [10] = "#a3be8c", /* green   */
-  [11] = "#ebcb8b", /* yellow  */
-  [12] = "#8fa1b3", /* blue    */
-  [13] = "#b48ead", /* magenta */
-  [14] = "#96b5b4", /* cyan    */
-  [15] = "#eff1f5", /* white   */
+  // [> 8 bright colors <]
+  // [8]  = "#65737e", [> black   <]
+  // [9]  = "#bf616a", [> red     <]
+  // [10] = "#a3be8c", [> green   <]
+  // [11] = "#ebcb8b", [> yellow  <]
+  // [12] = "#8fa1b3", [> blue    <]
+  // [13] = "#b48ead", [> magenta <]
+  // [14] = "#96b5b4", [> cyan    <]
+  // [15] = "#eff1f5", [> white   <]
 
-  /* special colors */
-  [256] = "#191c25", /* background */
-  [257] = "#c0c5ce", /* foreground */
-};
+  // [> special colors <]
+  // [256] = "#191c25", [> background <]
+  // [257] = "#c0c5ce", [> foreground <]
+// };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
