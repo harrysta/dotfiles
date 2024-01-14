39e98cb0b14b883f27c43b49f82929ea6736efb7
Harry Stafford <harrydstafford@gmail.com>
2024-01-14 17:00:43 +0200

    Update sb-weather

--- scripts/.local/bin/sb-weather.sh
+++ scripts/.local/bin/sb-weather.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-curl -sf "wttr.in?format=1" | tr -d "+" | awk 'NF{ print $NF }'
+
+EXPR='<span data-testid="TemperatureValue" class="CurrentConditions--tempValue--MHmYY">.*</span></span>'
+
+FAR=$(curl -s https://weather.com/weather/today/l/34.69,33.05?par=google | /usr/bin/grep -E -o "$EXPR" | tr -d -c 0-9 | head -c2)
+CEL=$(lua -e "print(math.floor(($FAR - 32) * 5 / 9 + 0.5))")
+echo "$CEL°C"
+
+# !curl -sf "wttr.in?format=1" | tr -d "+" | awk 'NF{ print $NF }'
